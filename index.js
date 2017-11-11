5b8c6d59f1e1a76ccd233de3007bc6b3f58f896e
Shea Lignitz <rlignitz@gmail.com>
2017-11-11 04:44:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,7 @@ function sayHiToGrandma(greeting) {
   if (greeting === whisper(greeting)) {
     return "I can't hear you!";
   }
+  else if (greeting === loud(greeting)) {
+    return "YES INDEED!";
+  }
 }
